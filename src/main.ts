759f96c444bb475dceb21d3b75cc545d008790fb
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-08-25 00:12:11 -0700

    Create base function declarations.

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+/**
+ * Imports placed here
+ */
+
+/**
+ * execute_discord_message sends the private discord message to the
+ * user to authenticate the user
+ */
+const execute_discord_message = (username: string): string => {
+  return "";
+};
+
+/**
+ * authenticate_user_information authenticates user information
+ * as well as their courses
+ */
+const authenticate_user_information = (user_input: string): string => {
+  return "";
+};
+
+/**
+ * determine_course_channel determines which courses
+ * to show which discord channel as well as roles
+ */
+const determine_course_channel = (user_courses: Array<string>) => {};
