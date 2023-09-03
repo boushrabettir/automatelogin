bccacfe7609070c97d88f25d8082d61584773a6e
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-09-02 23:35:59 -0700

    Validation input.

--- src/events/Interact.ts
+++ src/events/Interact.ts
@@ -1,5 +1,18 @@
 export class Interaction {
-    
+    private email: string;
+    private cwid: string;
+
+    public constructor(email: string, cwid: string) {
+        this.email = email;
+        this.cwid = cwid;
+    }
+
+
+    public validateInput = (): boolean => {
+        // TODO - Update this!!
+        const csufEmailPattern = /^[a-zA-Z0-9._%+-]+@csu\.fullerton\.edu$/;
+        return true ? (this.cwid.length !== 9 || csufEmailPattern.test(this.email) === false) : false;
+    }
 
 }
 
